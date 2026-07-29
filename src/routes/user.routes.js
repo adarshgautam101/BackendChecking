a9242f99a3ffe917af810417eb1d2b901a4542b2
ADARSH <adarsh.gautam@galaxyweblinks.com>
2026-07-29 14:57:57 +0530

    fix: correct multer upload field name for cover image in user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -24,7 +24,7 @@ router.route("/change-password").post(verifyJwt, changeCurrentPassword)
 router.route("/current-user").get(verifyJwt, getCurrentUser)
 router.route("/update-account").patch(verifyJwt, updateAccountDetails)
 router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJwt, upload.single("/coverimage"), updateUserCoverImg)
+router.route("/cover-image").patch(verifyJwt, upload.single("coverimage"), updateUserCoverImg)
 
 
 router.route("/c/:username").get(verifyJwt, getUserChannelProfile)
