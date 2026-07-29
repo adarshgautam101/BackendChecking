6de5357d9c7d19b935f0b02ffe1ff55b5f08dccc
ADARSH <adarsh.gautam@galaxyweblinks.com>
2026-07-29 14:10:05 +0530

    feat: add user management routes for password change, account update, and profile retrieval

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { refreshAccessToken, loginUser, logOutuser, registerUser } from "../controllers/user.controller.js";
+import { refreshAccessToken, loginUser, logOutuser, registerUser, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImg, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js"
+import { verify } from "jsonwebtoken";
 const router = Router();
 router.route("/register").post(
     upload.fields([
@@ -14,15 +15,18 @@ router.route("/register").post(
         }
     ]),
     registerUser)
-
-
 router.route("/login").post(upload.none(), loginUser)
-
-
-
 router.route("/logout").post(
     verifyJwt,
     logOutuser)
-
 router.route("/refresh_token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJwt, changeCurrentPassword)
+router.route("/current-user").get(verifyJwt, getCurrentUser)
+router.route("/update-account").patch(verifyJwt, updateAccountDetails)
+router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJwt, upload.single("/coverimage"), updateUserCoverImg)
+
+
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile)
+router.route("/history").get(verifyJwt, getWatchHistory)
 export default router
